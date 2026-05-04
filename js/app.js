b0fce0f613737c3dc3c0b820c14cbacf2ffb9b6d
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-04 16:34:10 +0530

    dashboard loaded to app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 console.log("app js loaded");
 function startClock() {
-
     function updateClock() {
         const now = new Date();
 
@@ -8,9 +7,9 @@ function startClock() {
         const currentTime = now.toLocaleTimeString();
 
         document.getElementById("live-date").innerHTML =
-          `<i class="fa-solid fa-calendar-days"></i> ${currentDate}`;
+            `<i class="fa-solid fa-calendar-days"></i> ${currentDate}`;
         document.getElementById("live-time").innerHTML =
-          `<i class="fa-solid fa-clock"></i> ${currentTime}`;
+            `<i class="fa-solid fa-clock"></i> ${currentTime}`;
     }
     updateClock();
     setInterval(updateClock, 1000);
@@ -21,12 +20,45 @@ function initApp() {
     renderNavbarStats();
     renderTerminalLogs();
 
-    if(appData.terminalLogs.length == 0) {
+    if (appData.terminalLogs.length == 0) {
         loadBootLogs();
     }
     startClock();
+    loadDashboard();
+    setupSidebarNavigation();
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     initApp();
 });
+
+function setupSidebarNavigation() {
+    let navItems = document.querySelectorAll(".sidebar li");
+
+    navItems.forEach((items) => {
+        items.addEventListener("click", function () {
+            navItems.forEach((Element) => {
+                Element.classList.remove("active");
+            });
+
+            items.classList.add("active");
+
+            const selectModule = items.dataset.module;
+
+            if (selectModule === "dashboard") {
+                loadDashboard();
+                console.log(selectModule);
+            } if (selectModule === "missions") {
+                loadMissions();
+            } if (selectModule === "focus") {
+                loadfocus();
+            } if (selectModule === "projects") {
+                loadProjects();
+            } if (selectModule === "analytics") {
+                loadAnalytics();
+            } if (selectModule === "notes") {
+                loadNotes();
+            }
+        });
+    });
+}
