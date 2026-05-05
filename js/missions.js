a719b6e0745ba60df789bc3dafaef9622369a7ef
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-05 10:23:31 +0530

    new feature added

--- js/missions.js
+++ js/missions.js
@@ -1,5 +1,6 @@
 function loadMissions() {
-    document.querySelector(".main-content").innerHTML = `<div class="mission-screen">
+    document.querySelector(".main-content").innerHTML =
+        `<div class="mission-screen">
 
         <h2>Mission Control</h2>
         <p>Manage your daily developer objectives</p>
@@ -7,4 +8,59 @@ function loadMissions() {
 
         <div id="mission-list"></div>
     </div>`;
+
+    renderMissionList();
+
+    const addMissionBtn = document.getElementById("add-mission-btn");
+
+    addMissionBtn.addEventListener("click", () => {
+        const missionTittle = prompt("Enter Mission Tittle");
+
+        if (!missionTittle) return;
+
+        appData.missions.push({
+            tittle: missionTittle,
+            completed: false
+        });
+        saveAppData();
+        renderMissionList();
+    });
+}
+
+function renderMissionList() {
+    const missionContainer = document.getElementById("mission-list");
+
+    missionContainer.innerHTML = "";
+
+    appData.missions.forEach((missions, index) => {
+        missionContainer.innerHTML += `
+            <div class="mission-card">
+                <h3>${missions.tittle}</h3>
+                <button data-index="${index}">Complete</button>
+            </div>
+        `;
+    });
+    setupMissionCompleteButtons();
+}
+
+function setupMissionCompleteButtons() {
+    const completeBtns = document.querySelectorAll(".mission-card button");
+
+    completeBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            const missionIndex = btn.dataset.index;
+
+            appData.missions[missionIndex].completed = true;
+
+            appData.xp += 20;
+
+            appData.terminalLogs.push("Mission completed +-20 XP awarded.");
+
+            saveAppData();
+
+            renderMissionList();
+            renderNavbarStats();
+            renderTerminalLogs();
+        });
+    });
 }
