6403ca893a9daf6e284dde8c46c7623e0218ea6c
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-06 23:25:03 +0530

    working on notes module

--- js/notes.js
+++ js/notes.js
@@ -1,16 +1,64 @@
 function loadNotesVault() {
-    const main = document.querySelector(".main-content");
+  const main = document.querySelector(".main-content");
 
-    main.innerHTML = `
-        <h2>Notes Vault</h2>
-        <p>Store quick dev notes and snippest
-        <textarea id="note-input" placeholder="Write your developer note..."></textarea>
-        <button id="save-note-btn">Save Note</button>
-        <div id="notes-list"> </div>
+  main.innerHTML = `
+        
+            <h2>Notes Vault</h2>
+            <p>Store quick dev notes and snippest
+            <textarea id="note-input" placeholder="Write your developer note..."></textarea>
+            <button id="save-note-btn">Save Note</button>
+            <div id="notes-list"> </div>
+        
     `;
+
+  const saveBtn = document.getElementById("save-note-btn");
+
+  saveBtn.addEventListener("click", () => {
+    console.log("save clicked");
+    const noteText = document.getElementById("note-input").value;
+
+    if (!noteText) return;
+
+    appData.notes.push({
+      content: noteText,
+    });
+
+    saveAppData();
     renderNotesList();
+
+    document.getElementById("note-input").value = "";
+  });
+
+  renderNotesList();
 }
 
 function renderNotesList() {
-    const notesList = document.getElementById("notes-list");
+  const notesList = document.getElementById("notes-list");
+
+  notesList.innerHTML = "";
+
+  appData.notes.forEach((notes, index) => {
+    notesList.innerHTML += `
+            <div class="note-card">
+                <p>${notes.content}</p>
+                <button data-delete-note="${index}">Delete</button>
+            </div>
+        `;
+  });
+  setupNoteDeleteButtons();
+}
+
+function setupNoteDeleteButtons() {
+  const deleteBtns = document.querySelectorAll("[data-delete-note]");
+
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const index = btn.dataset.deleteNote;
+
+      appData.notes.splice(index, 1);
+
+      saveAppData();
+      renderNotesList();
+    });
+  });
 }
